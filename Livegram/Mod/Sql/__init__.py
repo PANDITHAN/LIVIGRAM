603bd3d5bb496928a470106ed6a07e73220bfa4a
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2021-11-26 23:27:25 +0530

    🥸

--- Livegram/Mod/Sql/__init__.py
+++ Livegram/Mod/Sql/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from Livegram import Config.DB_URI
+from Livegram import Config
 
 
 def start() -> scoped_session:
